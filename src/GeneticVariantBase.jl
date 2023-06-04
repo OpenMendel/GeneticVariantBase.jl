b7eaa7241d1b6d404c8fde6f4a21ac7ead85a593
Seyoon Ko <y3kkoseyoon@gmail.com>
2023-06-03 22:43:00 -0700

    first commit

--- src/GeneticVariantBase.jl
+++ src/GeneticVariantBase.jl
@@ -1,5 +1,81 @@
 module GeneticVariantBase
+export GeneticData, Variant, VariantIterator, iterator
+export chrom, pos, rsid, alleles, alt_allele, ref_allele, maf, hwepval, infoscore
 
-# Write your package code here.
+abstract type GeneticData end
+abstract type Variant end
+abstract type VariantIterator end
+
+function Base.eltype(vi::VariantIterator)
+    Variant
+end
+
+"""
+    iterate(iter)
+Returns either a tuple of the first item and initial state or nothing if empty
+    iterate(iter, state)
+Returns either a tuple of the next item and next state or nothing if no items remain
+https://docs.julialang.org/en/v1/manual/interfaces/
+"""
+function Base.iterate(vi::VariantIterator,
+    state=(1, nothing))
+    return nothing
+end
+
+"""
+    length(variant_iterator)
+Returns the number of variants.
+"""
+function Base.length(vi::VariantIterator)::Int
+    0
+end
+
+function Base.size(vi::VariantIterator)
+    (Base.length(vi), )
+end
+
+"""
+    iterator(g)
+Creates a `VariantIterator` for `GeneticData` `g`.
+"""
+function iterator(g::GeneticData; startidx=1)
+    @assert false "Not implemented."
+end
+
+function chrom(g::GeneticData, v::Variant)
+    return 1
+end
+
+function pos(g::GeneticData, v::Variant)
+    return 0
+end
+
+function rsid(g::GeneticData, v::Variant)
+    return ""
+end
+
+function alleles(g::GeneticData, v::Variant)
+    return []
+end
+
+function alt_allele(g::GeneticData, v::Variant)
+    return ""
+end
+
+function ref_allele(g::GeneticData, v::Variant)
+    return ""
+end
+
+function maf(g::GeneticData, v::Variant)
+    return 0.0
+end
+
+function hwepval(g::GeneticData, v::Variant)
+    return 0.0
+end
+
+function infoscore(g::GeneticData, v::Variant)
+    return 0.0
+end
 
 end
