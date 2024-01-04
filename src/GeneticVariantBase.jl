f790e87ad687a686fbd2c4dee8b892c0e1f02320
Seyoon Ko <y3kkoseyoon@gmail.com>
2024-01-04 13:20:35 -0800

    what should we do with maf, hwepval, infoscore

--- src/GeneticVariantBase.jl
+++ src/GeneticVariantBase.jl
@@ -1,4 +1,5 @@
 module GeneticVariantBase
+using StatsBase
 export GeneticData, Variant, VariantIterator, iterator
 export chrom, pos, rsid, alleles, alt_allele, ref_allele, maf, hwepval, infoscore, load_values!
 
@@ -67,18 +68,49 @@ function ref_allele(g::GeneticData, v::Variant)::String
 end
 
 function maf(g::GeneticData, v::Variant)
-    return 0.0
+    arr = Vector{Float64}(undef, n_samples(g))
+    load_values!(arr, g, v)
+    maf(arr)
+end
+
+function maf(arr::AbstractArray{T}) where T <: Real
+    mean_level = mean(arr)
+    if mean_level < 1.0
+        return mean_level / 2
+    else
+        return (1 - mean_level / 2)
+    end
 end
 
 function hwepval(g::GeneticData, v::Variant)
+    arr = Vector{Float64}(undef, n_samples(g))
+    load_values!(arr, g, v)
+    hwepval(arr)
+end
+
+function hwepval(arr::AbstractArray{T}) where T <: Real
+    # Not applicaple for dosages. Only for genotypes or triplet probabilities (BGEN)
     return 0.0
 end
 
 function infoscore(g::GeneticData, v::Variant)
+    arr = Vector{Float64}(undef, n_samples(g))
+    load_values!(arr, g, v)
+    info_score(arr)
+end
+
+function infoscore(arr::AbstractArray{T}) where T <: Real
+    # TODO
     return 0.0
 end
 
-function load_values!(arr::AbstractArray, g::GeneticData, v::Variant)
+function load_alt_allele_dosages!(arr::AbstractArray{T}, g::GeneticData, v::Variant) where T <: Real
+    # real values between 0-2. 
+    return arr
+end
+
+function load_genotypes!(arr::AbstractArray{T}, g::GeneticData, v::Variant) where T <: Integer
+    # integer values between 0-2. 
     return arr
 end
 end
